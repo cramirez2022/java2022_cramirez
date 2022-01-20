a277fea02d3689186f8d7b55107d4efea72ea97b
Cèsar Ramirez <cesar.ramirez@componenteserviex.com>
2022-01-20 19:48:08 -0300

    version test

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+		
+public class Main {
+	public static void main(String[] args) {
+		
+	/*	int matriz[][] = new int[3][3];
+		
+		//Llenar valores
+		for (int x=0; x < matriz.length; x++) {
+			  for (int y=0; y < matriz[x].length; y++) {
+			    matriz[x][y] = x+y;
+			  }
+			}
+
+		
+		//mostrar valores
+		for (int x=0; x < matriz.length; x++) {
+			  for (int y=0; y < matriz[x].length; y++) {
+			    System.out.println (matriz[x][y]+ "" );
+			  }
+			}
+			*/
+
+
+		    int array[];
+		    array = new int[10]; 
+		    
+		    
+		    for (int j = 0; j < array.length; j++)
+		    {
+		        array[j] = j;
+		    }
+		    
+		      
+		    for (int j = 0; j < array.length; j++)
+		    {
+		        System.out.println(array[j] + " ");
+		    }
+		 }
+		
+	}
+
